6d9090bca20402be45358195a0b12ec31a42b804
Lozlant <lozlant@outlook.com>
2024-02-01 14:44:06 -0500

    Task2 complete: plane landing

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -11,6 +11,9 @@ public class Plane : MonoBehaviour
     Rigidbody2D rigidbody;
     Vector2 currentPosition;
     public float speed=1f;
+    public AnimationCurve landing;
+    public float timerValue;
+    
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
@@ -33,6 +36,17 @@ public class Plane : MonoBehaviour
     }
     private void Update()
     {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            timerValue += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(timerValue);
+            if(transform.localScale.z < 0.1f) 
+            {
+                Destroy(gameObject);
+            }
+
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
         lineRenderer.SetPosition(0, transform.position);
         if (points.Count > 0 )
         {
