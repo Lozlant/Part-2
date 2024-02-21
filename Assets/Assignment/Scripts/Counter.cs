504daffa062522e5d959e7e6d7d9404fc30c8aad
Lozlant <Lozlant@outlook.com>
2024-02-20 22:13:26 -0500

    task 2 completed: highlight the counter when mouse is over

--- Assets/Assignment/Scripts/Counter.cs
+++ Assets/Assignment/Scripts/Counter.cs
@@ -9,10 +9,13 @@ public class Counter : MonoBehaviour
 {
     public Player player;
     public float horFacing, verFacing;
+    public SpriteRenderer counterSpiritRenderer;
+    public Sprite highlightSprite;
+    Sprite sprite;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite=counterSpiritRenderer.sprite;
     }
 
     // Update is called once per frame
@@ -23,6 +26,15 @@ public class Counter : MonoBehaviour
     private void OnMouseDown()
     {
         player.setDestination((Vector2)transform.position,new Vector2(horFacing,verFacing));
+    }
+
+    private void OnMouseEnter()
+    {
+        counterSpiritRenderer.sprite = highlightSprite;
+    }
+    private void OnMouseExit()
+    {
+        counterSpiritRenderer.sprite = sprite;
     }
 
 
