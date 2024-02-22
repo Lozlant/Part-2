b620cffe7dbec4631fde92e9a05d636009cae576
Lozlant <Lozlant@outlook.com>
2024-02-22 02:51:58 -0500

    task2b completed: moving the selected Player

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Slider chargeSlider;
+    float charge;
+    public float maxCharge = 1;
+    Vector2 direction;
+
     public static PlayerMover selectedPlayer { get; private set; }
 
     public static void SetSelectedPlayer(PlayerMover player)
@@ -18,4 +23,38 @@ public class Controller : MonoBehaviour
         selectedPlayer = player;
         selectedPlayer.Selected(true);
     }
+
+    private void FixedUpdate()
+    {
+        if(selectedPlayer != null && direction!=Vector2.zero)
+        {
+            selectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            chargeSlider.value = charge;
+        }
+    }
+
+    private void Update()
+    {
+        if (selectedPlayer == null) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charge = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            charge+=Time.deltaTime;
+            charge=Mathf.Clamp(charge, 0,maxCharge);
+            chargeSlider.value = charge;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)selectedPlayer.transform.position).normalized * charge;
+        }
+
+    }
+
 }
--- Assets/Week 7/Scripts/PlayerMover.cs
+++ Assets/Week 7/Scripts/PlayerMover.cs
@@ -8,11 +8,16 @@ public class PlayerMover : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     Color normalColor;
+    Rigidbody2D rb;
+    public float speed = 500;
+
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer=GetComponent<SpriteRenderer>();
         normalColor=spriteRenderer.color;
+
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -35,5 +40,10 @@ public class PlayerMover : MonoBehaviour
     private void OnMouseDown()
     {
         Controller.SetSelectedPlayer(this);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction*speed);
     }
 }
